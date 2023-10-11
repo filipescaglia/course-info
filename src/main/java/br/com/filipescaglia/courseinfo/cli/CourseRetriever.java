ddf8fec2a8d9932a4c3992376e8a9c97295ac3c3
Filipe Scaglia <filipecscaglia@gmail.com>
2023-10-11 07:47:44 -0300

    CourseRetriever and SLF4J

---
+++ src/main/java/br/com/filipescaglia/courseinfo/cli/CourseRetriever.java
@@ -0,0 +1,28 @@
+package br.com.filipescaglia.courseinfo.cli;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CourseRetriever {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CourseRetriever.class);
+
+    public static void main(String[] args) {
+        LOG.info("CourseRetriever started!");
+        if (args.length == 0) {
+            LOG.warn("Please provide an authour name as first argument.");
+            return;
+        }
+
+        try {
+            retrieverCourses(args[0]);
+        } catch (Exception e) {
+            LOG.error("Unexpected error", e);
+        }
+    }
+
+    private static void retrieverCourses(String authorId) {
+        LOG.info("Retrieving courses for author '{}'", authorId);
+    }
+
+}
