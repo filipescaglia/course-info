bb8b0ee59e304a4d46646bf3eb72904591a7c4eb
Filipe Scaglia <filipecscaglia@gmail.com>
2023-10-19 06:55:03 -0300

    Parameterized unit test

--- src/main/java/br/com/filipescaglia/courseinfo/cli/services/PluralsightCourse.java
+++ src/main/java/br/com/filipescaglia/courseinfo/cli/services/PluralsightCourse.java
@@ -2,6 +2,13 @@ package br.com.filipescaglia.courseinfo.cli.services;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.time.Duration;
+import java.time.LocalTime;
+
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record PluralsightCourse(String id, String title, String duration, String contentUrl, boolean isRetired) {
+    public long durationInMinutes() {
+        return Duration.between(LocalTime.MIN, LocalTime.parse(duration()))
+                .toMinutes();
+    }
 }
